8921e820687ae6d9c897dea4db1ed38d4538e084
abhishekbh0476 <abhishekbh0476@gmail.com>
2022-11-10 16:07:29 +0530

    change1

---
+++ internal1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 16:04:04 2022
+
+@author: cse8
+"""
+
+import pandas as pd
+import numpy as np
+data=pd.read_csv('C:/Users/cse8/Desktop/olympic/athlete_events.csv')
+print(data)
